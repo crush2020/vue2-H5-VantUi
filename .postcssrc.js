6d2723266da6115dd7232a724c79498a20c15a81
crush <1937561927@qq.com>
2023-09-06 16:53:40 +0800

    修改插件生效浏览器范围

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,7 +3,7 @@
 module.exports = ({ file }) => {
   return {
     plugins: {
-      autoprefixer: {},
+      autoprefixer: { overrideBrowserslist: ['Android 4.1', 'iOS 7.1', 'Chrome > 31', 'ff > 31', 'ie >= 8'] },
       'postcss-px-to-viewport': {
         unitToConvert: 'px', // 要转化的单位
         viewportWidth: file.includes('vant') ? 375 : 750, // 视窗的宽度，对应的是我们设计稿的宽度，一般是750
